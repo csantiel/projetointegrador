8155ff2c3cfc39d443cb43bfbd2e4e73dd7f5531
Carlos <Carlos@CARLOS>
2018-10-19 19:23:19 -0300

    versão 1.2

---
+++ primeiraAPI/src/main/java/com/example/demo/controller/Autenticacao.java
@@ -0,0 +1,14 @@
+
+package com.example.demo.controller;
+
+import static com.sun.xml.internal.ws.api.message.Packet.Status.Request;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/login")
+public class Autenticacao {
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256)
+    public ResponseEntity autenticar(@RequestBody)
+}
--- primeiraAPI/src/main/java/com/example/demo/repository/ClienteRepository.java
+++ primeiraAPI/src/main/java/com/example/demo/repository/ClienteRepository.java
@@ -1,8 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Cliente;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
+    public Cliente findByNomeAndSenha (@Param("nome")String nome, @Param("senha")String senha);
 }
